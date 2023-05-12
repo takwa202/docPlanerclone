d05298e9de9dfc0f3925f0159b5ae82c7a4c3b5b
takwa202 <takwa.lassoued@esprit.tn>
2023-05-12 01:41:19 +0100

    cm

---
+++ js.js
@@ -0,0 +1,31 @@
+/********* filter function *****************/
+
+// function filterArray(arr) {
+// result = arr.filter(element => typeof element !== 'string')
+// return result
+
+// }
+
+/***************** sum abs      *******************/
+// arr =[1,10,-1,22]
+// function getAbsSum(arr) {
+//   var sum=0;
+//   for(var i of arr){
+//     sum += Math.abs(i);
+//   }
+//   return sum;
+
+// }
+/************************ true false sum ***********************/
+// arr =[true, false, false, true, false];
+// function countTrue(arr) {
+//   var sum =0;
+//   for (var i of arr) {
+//     if (i) {
+//       sum+=1;
+//     }
+   
+//   }
+//   return sum;
+// }
+// console.log( countTrue(arr));
